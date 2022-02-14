c6d59b4bbc72eb6680130532683e65c121182fe4
Alexander Matyasko <alexander.matyasko@gmail.com>
2022-02-14 15:16:47 +0800

    Add cifar100 dataset

--- utils.py
+++ utils.py
@@ -99,6 +99,14 @@ def cifar10(root):
         'test': {'data': test_set.data, 'labels': test_set.targets}
     }
 
+def cifar100(root):
+    train_set = torchvision.datasets.CIFAR100(root=root, train=True, download=True)
+    test_set = torchvision.datasets.CIFAR100(root=root, train=False, download=True)
+    return {
+        'train': {'data': train_set.data, 'labels': train_set.targets},
+        'test': {'data': test_set.data, 'labels': test_set.targets}
+    }
+
 #####################
 ## data loading
 #####################
