2198b7666373e376402f3ffd98ea9dd37414b94b
Alexander Matyasko <alexander.matyasko@gmail.com>
2022-02-14 17:03:01 +0800

    Add svhn dataset

--- utils.py
+++ utils.py
@@ -107,6 +107,14 @@ def cifar100(root):
         'test': {'data': test_set.data, 'labels': test_set.targets}
     }
 
+def svhn(root):
+    train_set = torchvision.datasets.SVHN(root=root, split='train', download=True)
+    test_set = torchvision.datasets.SVHN(root=root, split='test', download=True)
+    return {
+        'train': {'data': train_set.data, 'labels': train_set.targets},
+        'test': {'data': test_set.data, 'labels': test_set.targets}
+    }
+
 #####################
 ## data loading
 #####################
