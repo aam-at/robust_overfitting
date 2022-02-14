eea1669a3dc41c091bd53161c75a25d47a0e7a00
Alexander Matyasko <alexander.matyasko@gmail.com>
2022-02-14 16:47:55 +0800

    Update svhn script

--- train_svhn.py
+++ train_svhn.py
@@ -10,11 +10,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import datasets, transforms
+from PIL import Image
 
 import os
 
 from wideresnet import WideResNet
 from preactresnet import PreActResNet18
+from utils import *
 
 svhn_mean = (0.5, 0.5, 0.5)
 svhn_std = (0.5, 0.5, 0.5)
@@ -30,6 +32,23 @@ upper_limit, lower_limit = 1,0
 
 def clamp(X, lower_limit, upper_limit):
     return torch.max(torch.min(X, upper_limit), lower_limit)
+
+
+class NumpyDataset(torch.utils.data.Dataset):
+    def __init__(self, x, y, transform):
+        assert len(x) == len(y)
+        self.x = x
+        self.y = y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x = Image.fromarray(self.x[index])
+        y = self.y[index]
+        return self.transform(x), y
+
+    def __len__(self):
+        return len(self.x)
+
 
 def mixup_data(x, y, alpha=1.0):
     '''Returns mixed inputs, pairs of targets, and lambda'''
@@ -110,14 +129,18 @@ def get_args():
     parser.add_argument('--batch-size', default=128, type=int)
     parser.add_argument('--data-dir', default='../svhn-data', type=str)
     parser.add_argument('--epochs', default=200, type=int)
-    parser.add_argument('--lr-schedule', default='piecewise', choices=['superconverge', 'piecewise'])
-    parser.add_argument('--lr-max', default=0.01, type=float)
-    parser.add_argument('--attack', default='pgd', type=str, choices=['pgd', 'none'])
+    parser.add_argument('--lr-schedule', default='piecewise', choices=['superconverge', 'piecewise', 'linear', 'piecewisesmoothed', 'piecewisezoom', 'onedrop', 'multipledecay', 'cosine'])
+    parser.add_argument('--lr-max', default=0.1, type=float)
+    parser.add_argument('--lr-one-drop', default=0.01, type=float)
+    parser.add_argument('--lr-drop-epoch', default=100, type=int)
+    parser.add_argument('--attack', default='pgd', type=str, choices=['pgd', 'fgsm', 'free', 'none'])
     parser.add_argument('--epsilon', default=8, type=int)
     parser.add_argument('--attack-iters', default=10, type=int)
     parser.add_argument('--restarts', default=1, type=int)
     parser.add_argument('--pgd-alpha', default=2, type=float)
+    parser.add_argument('--fgsm-alpha', default=1.25, type=float)
     parser.add_argument('--norm', default='l_inf', type=str, choices=['l_inf', 'l_2'])
+    parser.add_argument('--fgsm-init', default='random', choices=['zero', 'random', 'previous'])
     parser.add_argument('--fname', default='svhn_model', type=str)
     parser.add_argument('--seed', default=0, type=int)
     parser.add_argument('--half', action='store_true')
@@ -128,7 +151,8 @@ def get_args():
     parser.add_argument('--mixup', action='store_true')
     parser.add_argument('--mixup-alpha', type=float)
     parser.add_argument('--eval', action='store_true')
-    parser.add_argument('--chkpt-iters', default=10, type=int)
+    parser.add_argument('--val', action='store_true')
+    parser.add_argument('--chkpt-iters', default=1, type=int)
     return parser.parse_args()
 
 
@@ -158,15 +182,31 @@ def main():
         transforms.ToTensor(),
         # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
+    if args.cutout:
+        train_transform.append(Cutout(args.cutout_len, args.cutout_len))
     test_transform = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
     num_workers = 2
-    train_dataset = datasets.SVHN(
-        args.data_dir, split='train', transform=train_transform, download=True)
-    test_dataset = datasets.SVHN(
-        args.data_dir, split='test', transform=test_transform, download=True)
+    if args.val:
+        try:
+            print("Loading validation split")
+            dataset = torch.load("cifar100_validation_split.pth")
+        except:
+            print("Couldn't find a dataset with a validation split, did you run "
+                  "generate_cifar100_validation.py?")
+            return
+        val_dataset = NumpyDataset(dataset['val']['data'], dataset['val']['labels'], test_transform)
+        val_loader = torch.utils.data.DataLoader(
+            dataset=val_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            pin_memory=True,
+            num_workers=2,
+        )
+    else:
+        dataset = cifar100(args.data_dir)
+    train_dataset = NumpyDataset(dataset['train']['data'], dataset['train']['labels'], train_transform)
     train_loader = torch.utils.data.DataLoader(
         dataset=train_dataset,
         batch_size=args.batch_size,
@@ -174,6 +214,8 @@ def main():
         pin_memory=True,
         num_workers=num_workers,
     )
+
+    test_dataset = NumpyDataset(dataset['test']['data'], dataset['test']['labels'], test_transform)
     test_loader = torch.utils.data.DataLoader(
         dataset=test_dataset,
         batch_size=args.batch_size,
@@ -185,16 +227,14 @@ def main():
     epsilon = (args.epsilon / 255.)
     pgd_alpha = (args.pgd_alpha / 255.)
 
-    # model = models_dict[args.architecture]().cuda()
-    # model.apply(initialize_weights)
     if args.model == 'PreActResNet18':
         model = PreActResNet18()
     elif args.model == 'WideResNet':
-        model = WideResNet(34, 10, widen_factor=args.width_factor, dropRate=0.0)
+        model = WideResNet(28, 10, widen_factor=args.width_factor, dropRate=0.0)
     else:
         raise ValueError("Unknown model")
 
-    model = model.cuda()
+    model = nn.DataParallel(model).cuda()
     model.train()
 
     if args.l2:
@@ -213,11 +253,20 @@ def main():
 
     criterion = nn.CrossEntropyLoss()
 
-    epochs = args.epochs
+    if args.attack == 'free':
+        delta = torch.zeros(args.batch_size, 3, 32, 32).cuda()
+        delta.requires_grad = True
+    elif args.attack == 'fgsm' and args.fgsm_init == 'previous':
+        delta = torch.zeros(args.batch_size, 3, 32, 32).cuda()
+        delta.requires_grad = True
+
+    if args.attack == 'free':
+        epochs = int(math.ceil(args.epochs / args.attack_iters))
+    else:
+        epochs = args.epochs
 
     if args.lr_schedule == 'superconverge':
         lr_schedule = lambda t: np.interp([t], [0, args.epochs * 2 // 5, args.epochs], [0, args.lr_max, 0])[0]
-        # lr_schedule = lambda t: np.interp([t], [0, args.epochs], [0, args.lr_max])[0]
     elif args.lr_schedule == 'piecewise':
         def lr_schedule(t):
             if t / args.epochs < 0.5:
@@ -226,15 +275,35 @@ def main():
                 return args.lr_max / 10.
             else:
                 return args.lr_max / 100.
+    elif args.lr_schedule == 'linear':
+        lr_schedule = lambda t: np.interp([t], [0, args.epochs // 3, args.epochs * 2 // 3, args.epochs], [args.lr_max, args.lr_max, args.lr_max / 10, args.lr_max / 100])[0]
+    elif args.lr_schedule == 'onedrop':
+        def lr_schedule(t):
+            if t < args.lr_drop_epoch:
+                return args.lr_max
+            else:
+                return args.lr_one_drop
+    elif args.lr_schedule == 'multipledecay':
+        def lr_schedule(t):
+            return args.lr_max - (t//(args.epochs//10))*(args.lr_max/10)
+    elif args.lr_schedule == 'cosine': 
+        def lr_schedule(t): 
+            return args.lr_max * 0.5 * (1 + np.cos(t / args.epochs * np.pi))
 
+
+    best_test_robust_acc = 0
+    best_val_robust_acc = 0
     if args.resume:
         start_epoch = args.resume
         model.load_state_dict(torch.load(os.path.join(args.fname, f'model_{start_epoch-1}.pth')))
         opt.load_state_dict(torch.load(os.path.join(args.fname, f'opt_{start_epoch-1}.pth')))
         logger.info(f'Resuming at epoch {start_epoch}')
+
+        best_test_robust_acc = torch.load(os.path.join(args.fname, f'model_best.pth'))['test_robust_acc']
+        if args.val:
+            best_val_robust_acc = torch.load(os.path.join(args.fname, f'model_val.pth'))['val_robust_acc']
     else:
         start_epoch = 0
-
 
     if args.eval:
         if not args.resume:
@@ -252,9 +321,9 @@ def main():
         train_robust_acc = 0
         train_n = 0
         for i, (X, y) in enumerate(train_loader):
+            X, y = X.cuda(), y.cuda()
             if args.eval:
                 break
-            X, y = X.cuda(), y.cuda()
             if args.mixup:
                 X, y_a, y_b, lam = mixup_data(X, y, args.mixup_alpha)
                 X, y_a, y_b = map(Variable, (X, y_a, y_b))
@@ -268,7 +337,8 @@ def main():
                 else:
                     delta = attack_pgd(model, X, y, epsilon, pgd_alpha, args.attack_iters, args.restarts, args.norm)
                 delta = delta.detach()
-
+            elif args.attack == 'fgsm':
+                delta = attack_pgd(model, X, y, epsilon, args.fgsm_alpha*epsilon, 1, 1, args.norm)
             # Standard training
             elif args.attack == 'none':
                 delta = torch.zeros_like(X)
@@ -310,7 +380,6 @@ def main():
         test_n = 0
         for i, (X, y) in enumerate(test_loader):
             X, y = X.cuda(), y.cuda()
-
             # Random initialization
             if args.attack == 'none':
                 delta = torch.zeros_like(X)
@@ -331,15 +400,74 @@ def main():
             test_n += y.size(0)
 
         test_time = time.time()
-        if not args.eval: 
+
+        if args.val:
+            val_loss = 0
+            val_acc = 0
+            val_robust_loss = 0
+            val_robust_acc = 0
+            val_n = 0
+            for i, (X, y) in enumerate(val_loader):
+                X, y = X.cuda(), y.cuda()
+                # Random initialization
+                if args.attack == 'none':
+                    delta = torch.zeros_like(X)
+                else:
+                    delta = attack_pgd(model, X, y, epsilon, pgd_alpha, args.attack_iters, args.restarts, args.norm, early_stop=args.eval)
+                delta = delta.detach()
+
+                robust_output = model(normalize(torch.clamp(X + delta[:X.size(0)], min=lower_limit, max=upper_limit)))
+                robust_loss = criterion(robust_output, y)
+
+                output = model(normalize(X))
+                loss = criterion(output, y)
+
+                val_robust_loss += robust_loss.item() * y.size(0)
+                val_robust_acc += (robust_output.max(1)[1] == y).sum().item()
+                val_loss += loss.item() * y.size(0)
+                val_acc += (output.max(1)[1] == y).sum().item()
+                val_n += y.size(0)
+
+        if not args.eval:
             logger.info('%d \t %.1f \t \t %.1f \t \t %.4f \t %.4f \t %.4f \t %.4f \t \t %.4f \t \t %.4f \t %.4f \t %.4f \t \t %.4f',
                 epoch, train_time - start_time, test_time - train_time, lr,
                 train_loss/train_n, train_acc/train_n, train_robust_loss/train_n, train_robust_acc/train_n,
                 test_loss/test_n, test_acc/test_n, test_robust_loss/test_n, test_robust_acc/test_n)
-            
+
+            if args.val:
+                logger.info('validation %.4f \t %.4f \t %.4f \t %.4f',
+                    val_loss/val_n, val_acc/val_n, val_robust_loss/val_n, val_robust_acc/val_n)
+
+                if val_robust_acc/val_n > best_val_robust_acc:
+                    torch.save({
+                            'state_dict':model.state_dict(),
+                            'test_robust_acc':test_robust_acc/test_n,
+                            'test_robust_loss':test_robust_loss/test_n,
+                            'test_loss':test_loss/test_n,
+                            'test_acc':test_acc/test_n,
+                            'val_robust_acc':val_robust_acc/val_n,
+                            'val_robust_loss':val_robust_loss/val_n,
+                            'val_loss':val_loss/val_n,
+                            'val_acc':val_acc/val_n,
+                        }, os.path.join(args.fname, f'model_val.pth'))
+                    best_val_robust_acc = val_robust_acc/val_n
+
+            # save checkpoint
             if (epoch+1) % args.chkpt_iters == 0 or epoch+1 == epochs:
                 torch.save(model.state_dict(), os.path.join(args.fname, f'model_{epoch}.pth'))
                 torch.save(opt.state_dict(), os.path.join(args.fname, f'opt_{epoch}.pth'))
+
+            # save best
+            if test_robust_acc/test_n > best_test_robust_acc:
+                torch.save({
+                        'state_dict':model.state_dict(),
+                        'test_robust_acc':test_robust_acc/test_n,
+                        'test_robust_loss':test_robust_loss/test_n,
+                        'test_loss':test_loss/test_n,
+                        'test_acc':test_acc/test_n,
+                        'epoch':epoch
+                    }, os.path.join(args.fname, f'model_best.pth'))
+                best_test_robust_acc = test_robust_acc/test_n
         else:
             logger.info('%d \t %.1f \t \t %.1f \t \t %.4f \t %.4f \t %.4f \t %.4f \t \t %.4f \t \t %.4f \t %.4f \t %.4f \t \t %.4f',
                 epoch, train_time - start_time, test_time - train_time, -1,
